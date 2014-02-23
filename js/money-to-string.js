6e5fdb040b231a360b73b37c471ff8429d1b70df
Longmoon <chris@longmoon.com>
2014-02-23 13:52:14 -0800

    Created engNum object and convert() placeholder

--- js/money-to-string.js
+++ js/money-to-string.js
@@ -1,12 +1,30 @@
 $(function(){
 	
-	var input = "";
+	var strAmt = "";
 	
+	var engNums = {
+		"ones" : [
+			"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+		],
+		"teens" : [
+			"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"		   
+		],
+		"tens" : [
+			"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"		  
+		],
+		"ordermag" : [
+			"hundred", "thousand", "million", "billion", "trillion", "zillion"			  
+		]
+	}
+	
+	var convert = function(strAmt) {
+		return strAmt;	
+	}
 	
 	$("#amount").change(
 		function() {
-			var input = $(this).val();
-			$("#output").text(input);
+			var strAmt = $(this).val().toString();
+			$("#output").text(convert(strAmt));
 		}	
 	);
 	
