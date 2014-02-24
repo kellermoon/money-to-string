1d93bc1527117949fd80f772ee2b329c1bba0788
Longmoon <chris@longmoon.com>
2014-02-23 17:31:29 -0800

    Get dollars and cents from input amount.
    I seem to have re-invented form input validation and I fear it is quite
    fragile.

--- js/money-to-string.js
+++ js/money-to-string.js
@@ -1,6 +1,10 @@
 $(function(){
 	
-	var strAmt = "";
+	var amount;
+	var dollars;
+	var strDollars;
+	var cents;
+	var strCents;
 	
 	var engNums = {
 		"ones" : [
@@ -17,14 +21,47 @@ $(function(){
 		]
 	}
 	
-	var convert = function(strAmt) {
-		return strAmt;	
+	var convertToDollarsAndCents = function(amount) {
+		
+		// If amount is NaN or empty string, reject it.
+		if(isNaN(amount) || amount == "") return false;
+		
+		var splStrAmt = amount.toString().split(".");
+				
+		// If amount is negative, or cents has more than two digits (i.e. >99), reject the amount.
+		if(amount < 0) {
+			$("#amount").val(undefined);
+			return false;
+		}
+		if(parseInt(splStrAmt[1]) > 99) return false;
+		
+		// Set dollars.
+		dollars = parseInt(splStrAmt[0]);
+		strDollars = dollars.toString();
+		
+		// Set cents -- handle various valid values accordingly.
+		cents = parseInt(splStrAmt[1]);
+		if(!cents || isNaN(cents) || cents == 0) {
+			cents = 0;
+			strCents = "00"
+		}
+		if(cents < 9) {
+			strCents = cents + "0";
+		} else {
+			strCents = cents;
+		}
+		
+		return true;
 	}
 	
-	$("#amount").change(
+	$("#amount").focus().change(
 		function() {
-			var strAmt = $(this).val().toString();
-			$("#output").text(convert(strAmt));
+			var amount = $(this).val();
+			if(convertToDollarsAndCents(amount)) {
+				$("#output").text(strDollars + " dollars and " + strCents + " cents.");
+			} else {
+				$("#output").text("Please format your reponse properly, and make sure it's not a negative value.");
+			}
 		}	
 	);
 	
